06e408bde4a9d2e005d8867b6d17cdaa81a279aa
john-smith8888 <johnsmith8aug88@gmail.com>
2024-11-07 14:44:14 +0200

    Changed legibilty of code and renamed variables to ease understanding.

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,25 +12,27 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+
+		// variables initialization, cutoff for AM/PM calculation:
         int minutesTilNoon = 12*60;
 		int hoursShort = hours;
-		String m = "";
+		String ampm = "";
 		String sMinutes = "" + minutes;
-		//Indication of PM
+		//Indication of AM/PM
 		// int pmFlag = (hours - hoursShort) / 12;
 		// int amFlag = 1 - pmFlag;
+
 		if (hours * 60 + minutes >= minutesTilNoon ) {
-			m = "P";
+			ampm = "P";
 			if (hours ==12) {hoursShort=12;}
 			else hoursShort = hours % 12;
 		}
-		else m="A";
-		m = m + "M";
+		else ampm="A";
+		ampm = ampm + "M";
+		// Dealing with single-digit minutes:
 		if (minutes<10) {
 			sMinutes = "0" + minutes;
 		}
-		// System.out.println("Hours: " + hours + " Minutes: " + sMinutes + " hoursShort: " + hoursShort);
-		// String construction
-		System.out.println(hoursShort + ":" + sMinutes + " "+ m);
+		System.out.println(hoursShort + ":" + sMinutes + " "+ ampm);
 	}
 }
