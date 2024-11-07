1de457e186d275cba60a3415a168aec96524f66f
john-smith8888 <johnsmith8aug88@gmail.com>
2024-11-07 14:58:43 +0200

    Changed names order in Bill3 assignment.

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3 {
 		// Portion Calculation
 		double portion = Math.ceil(total / 3.0);
 		
-		System.out.println("Dear " + name1 + ", "+ name2 + ", and " + name3
+		System.out.println("Dear " + name3 + ", "+ name2 + ", and " + name1
 		 + ": pay "+ portion + " Shekels each.");
 	}
 }
